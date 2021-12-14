94f72c21b0126e48ebc49d17526690743ff5ccec
Zeeshan Haider <zhadier39@gmail.com>
2021-12-14 22:55:05 +0500

    Add function to create dynamic html

--- index.js
+++ index.js
@@ -1,3 +1,93 @@
+
+// Works Section Dynamically Created
+const worksSection = document.querySelector('#Works');
+function creatProject (project) {
+  worksSection.innerHTML += `
+  <article class="card ${project.name}">
+  <img id="p1image" src="${project.featuredImage}" alt="${project.name} Image">
+  <div class="card-body">
+      <div class="project-Head">
+          <h3>${project.description}</h3>
+          <p></p>
+      </div>
+      <ul>
+          <li><a href="https://en.wikipedia.org/wiki/CSS" class="program-Link"></a></li>
+          <li><a href="https://en.wikipedia.org/wiki/HTML" class="program-Link"></a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)"
+                  class="program-Link"></a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Ruby" class="program-Link"></a></li>
+      </ul>
+      <button class="project-Button"></button>
+  </div>
+</article>`;
+}
+const projectsArray = [
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+    technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p1.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project1',
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p1d.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project2',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p2d.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project3',
+  },
+  {
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p3d.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project4',
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/proj-img.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project5',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p2d.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project6',
+  },
+  {
+    name: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    featuredImage: 'images/p3d.svg',
+    linkToLiveVersion: 'https://zhadier.github.io/My-Portofolio/',
+    linkToSource: 'https://github.com/zhadier/My-Portofolio',
+    class: 'project7',
+  },
+];
+
+
 // Mobile menu (Dynamic HTML)
 const openMenu = document.querySelector('.nav-Button');
 const closeMenu = document.querySelector('.nav-Button-Close');
@@ -17,71 +107,3 @@ menuLinks.forEach((li) => {
     menu.style.top = '-150%';
   });
 });
-
-// Works Section Dynamically Created
-
-const projectsArray = [
-  {
-    name: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
-    technologies: ['css', 'html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project1',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project2',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project3',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project4',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project5',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project6',
-  },
-  {
-    name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    technologies: ['html', 'Bootstrap', 'Ruby'],
-    featuredImage: 'images/p1.svg',
-    linkToLiveVersion: null,
-    linkToSource: null,
-    class: 'project7',
-  },
-];
